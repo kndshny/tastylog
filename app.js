fe4012578fda6cb3111702352e964cfa11c575e4
kndshny <kndshny721@gmail.com>
2024-03-31 15:22:06 +0900

    Add --log output func --console, app, access

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const logger = require('./lib/log/logger.js')
+const accesslogger = require('./lib/log/accesslogger.js')
+const applicaitonlogger=require('./lib/log/applicationlogger.js')
 const express = require('express');
 const favicon = require('serve-favicon');
 const app = express();
@@ -9,14 +11,20 @@ const app = express();
 app.set('view engine', 'ejs');
 app.disable('x-powered-by')
 
-// Static resource rooting.
+// Static resource rooting
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
-// Dynamic resource rooting.
+// Set access log
+app.use(accesslogger())
+
+// Dynamic resource rooting
 app.use('/', require('./routes/index.js'));
 
-// Execute web application.
+// Set application log
+app.use(applicaitonlogger())
+
+// Execute web application
 app.listen(PORT, () => {
-	logger.console.info(`Application listening at :${PORT}`);
+	logger.applicaiton.info(`Application listening at :${PORT}`);
 });
--- config/log4js.config.js
+++ config/log4js.config.js
@@ -1,13 +1,47 @@
+const path = require('path');
+const LOG_ROOT_DIR =
+	process.env.LOG_ROOT_DIR || path.join(__dirname, '../logs');
+
 module.exports = {
-  appenders: {
-    consoleLogAppender:{
-      type: 'console'
-    }
-  },
-  categories: {
-    default: {
-      appenders: ['consoleLogAppender'],
-      level: 'ALL'
-    }
-  },
+	appenders: {
+		ConsoleLogAppender: {
+			type: 'console',
+		},
+		ApplicationLogAppender: {
+			type: 'dateFile',
+			filename: path.join(LOG_ROOT_DIR, './application.log'),
+			pattern: 'yyyyMMdd',
+			numBackups: 7, // 'daysToKeep' の代わりに 'numBackups' を使用
+			compress: true, // 古いログファイルを圧縮
+		},
+		AccessLogAppender: {
+			type: 'dateFile',
+			filename: path.join(LOG_ROOT_DIR, './application.log'),
+			pattern: 'yyyyMMdd',
+			numBackups: 7, // 'daysToKeep' の代わりに 'numBackups' を使用
+			compress: true, // 古いログファイルを圧縮
+		},
+	},
+	categories: {
+		default: {
+      appenders: [
+        'ConsoleLogAppender'
+      ],
+			level: 'ALL',
+		},
+		applicaiton: {
+      appenders: [
+        'ApplicationLogAppender',
+        'ConsoleLogAppender'
+      ],
+			level: 'INFO',
+		},
+		access: {
+      appenders: [
+        'AccessLogAppender',
+        'ConsoleLogAppender'
+      ],
+			level: 'INFO',
+		},
+	},
 };
